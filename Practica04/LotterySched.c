0d6d9fc2de28461623183d4e526572b9c597051d
Yoba7-7 <lgma77@ciencias.unam.mx>
2024-04-26 21:00:07 -0600

    Me rindo, se queda ciclado

--- Practica04/LotterySched.c
+++ Practica04/LotterySched.c
@@ -1,97 +1,95 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <time.h>
 
-#define NRO_PROCESOS = 5
-#define QUANTUM_INIC = 10
-#define QUANTUM_FINAL = 10
-#define NRO_TICKETS = 100
+#define NUM_PROCESOS 5
+#define QUANTUM_TOTAL 10
 
-struct Proceso
-{
-	int id;
-	int tickets;
-	int quantum;
-	int tiempo_rest;
-	int estado;
-	int posicion;
-};
+typedef struct {
+    int id;
+    int quantum;
+    int tiempo_cpu;
+    int boletos;
+    int *numeros;
+    int estado; // 0: En espera, 1: Ejecutando, 2: Terminado
+} Proceso;
 
-void imprime_progreso(int tiempo_rest, int tiempo_total)
-{
-	printf("Ejecutando proceso: %d de %d. Tiempo restante. \n", tiempo_total - tiempo_rest + 1, tiempo_total);
+Proceso procesos[NUM_PROCESOS];
+
+void asignar_recursos() {
+    srand(time(NULL));
+    for (int i = 0; i < NUM_PROCESOS; i++) {
+        procesos[i].id = i + 1; // Asignar un ID único a cada proceso
+        procesos[i].quantum = rand() % (QUANTUM_TOTAL / 2) + 1; // Quantum aleatorio
+        procesos[i].tiempo_cpu = 0;
+        procesos[i].boletos = rand() % 10 + 1; // Número aleatorio de boletos
+        procesos[i].numeros = (int *)malloc(sizeof(int) * procesos[i].boletos);
+        for (int j = 0; j < procesos[i].boletos; j++) {
+            procesos[i].numeros[j] = rand() % 100; // Números aleatorios
+        }
+        procesos[i].estado = 0; // Estado inicial: En espera
+    }
 }
 
-void schedule(struct Proceso procs[], int num_procs)
-{
-	int tickets_totales = 0;
-	int i , ganador;
-	int id_ganador = 0;
+void imprimir_proceso(Proceso p) {
+    printf("Proceso ID: %d | Estado: %d | Quantum restante: %d | Tiempo restante: %d\n", p.id, p.estado, p.quantum, QUANTUM_TOTAL - p.tiempo_cpu);
+}
 
-	//calucla el numero total de tickets
-	for (int i = 0; i < num_procs; ++i)
-	{
-		tickets_totales += procs[i].tickets;
-	}
+void imprimir_detalles() {
+    for (int i = 0; i < NUM_PROCESOS; i++) {
+        imprimir_proceso(procesos[i]);
+    }
+}
 
-	//selecciona un numero ganador
-	ganador = rand() % tickets_totales;
+int rifa_loteria() {
+    int total_boletos = 0;
+    for (int i = 0; i < NUM_PROCESOS; i++) {
+        total_boletos += procesos[i].boletos;
+    }
+    return rand() % total_boletos;
+}
 
-	//encuentra el ticket ganador
-	for (int i = 0; i < num_procs; ++i)
-	{
-		if (ganador < procs[i].tickets && procs[i].estado == 1)
-		{
-			printf("Proceso ganador: %d \n" , procs[i].id);
-			id_ganador = procs[i].id;
-			break;
-		}
-		ganador -= procs[i].tickets,
-	}
+void ejecutar_proceso(int ganador) {
+    int boletos_acumulados = 0;
+    for (int i = 0; i < NUM_PROCESOS; i++) {
+        boletos_acumulados += procesos[i].boletos;
+        if (ganador < boletos_acumulados) {
+            printf("Ejecutando proceso ID: %d\n", procesos[i].id);
+            procesos[i].estado = 1; // Estado: Ejecutando
+            procesos[i].quantum--;
+            procesos[i].tiempo_cpu++;
+            if (procesos[i].quantum == 0) {
+                if (procesos[i].tiempo_cpu < QUANTUM_TOTAL) {
+                    printf("Proceso ID: %d utiliza todo su quantum, pero aún no ha terminado.\n", procesos[i].id);
+                } else {
+                    printf("Proceso ID: %d termina su ejecución.\n", procesos[i].id);
+                    procesos[i].estado = 2; // Estado: Terminado
+                    free(procesos[i].numeros); // Liberar memoria aquí
+                }
+            }
+            break;
+        }
+    }
+}
 
-	// ejecutar el proceso ganador
-	printf("Ejecutando el proceso %d ...\n", id_ganador);
-	for (int i = 0; i < num_procs; ++i)
-	{
-		imprime_progreso(procs[id_ganador].tiempo_rest, procs[id_ganador].quantum);
-		sleep(1);
-		procs[id_ganador].tiempo_rest --;
-		procs[id_ganador].tickets = 0;
-		if (procs[id_ganador].tiempo_rest <= 0)
-		{
-			procs[id_ganador].estado = 0;
-			printf("Proceso terminado, se ha ejecutado con exito.\n Procesos por ejecutar: ");
-			for (int j = 0; j < NRO_PROCESOS; ++j)
-			{
-				if (procs[j].estado == 1)
-				{
-					printf("Proceso: %d" , procs[j].id);
-				}
-			}
-		}
-	}
+int main() {
+    asignar_recursos();
+    int todos_terminados = 0; // Cambiamos a 0 para que el bucle se ejecute al menos una vez
+    while (!todos_terminados) { // Verificamos si todos los procesos han terminado
+        imprimir_detalles();
+        int ganador = rifa_loteria();
+        ejecutar_proceso(ganador);
+        // Verificamos si todos los procesos han terminado
+        todos_terminados = 1;
+        for (int i = 0; i < NUM_PROCESOS; i++) {
+            if (procesos[i].estado != 2) {
+                todos_terminados = 0;
+                break;
+            }
+        }
+    }
 
+    printf("Todos los procesos han terminado. Finalizando el programa.\n");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return 0;
 }
